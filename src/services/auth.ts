a9a1fe2645eb67556258b6d17b81a992b4939de1
Sudheer Kumar <masinasudheerkumar@gmail.com>
2022-05-12 16:16:05 +0000

    error handling changes

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,20 +1,22 @@
-import { Service, Inject } from "typedi";
-import jwt from "jsonwebtoken";
+import { Service, Inject } from 'typedi';
+import jwt from 'jsonwebtoken';
 import config from '@/config';
-import { randomBytes, pbkdf2Sync } from "crypto";
-import { IUser, IUserInputDTO } from "@/interfaces/IUser";
+import { randomBytes, pbkdf2Sync } from 'crypto';
+import { IUser, IUserInputDTO } from '@/interfaces/IUser';
+import { APIError } from '@/loaders/errors';
+import { HttpStatusCode } from '@/config/constants';
 
 @Service()
 export default class AuthService {
     constructor(
-        @Inject("userModel") private userModel: Models.UserModel,
-        @Inject("logger") private logger
-    ){};
+        @Inject('userModel') private userModel: Models.UserModel,
+        @Inject('logger') private logger
+    ) { };
 
-    public async SingUp(userInputDTO: IUserInputDTO): Promise<{ user: IUser; token: string}>{
+    public async SingUp(userInputDTO: IUserInputDTO): Promise<{ user: IUser; token: string }> {
         try {
             const salt = randomBytes(32).toString('hex');
-            const password =  pbkdf2Sync(userInputDTO.password, salt, 1000, 64, 'sha512').toString("hex");
+            const password = pbkdf2Sync(userInputDTO.password, salt, 1000, 64, 'sha512').toString('hex');
 
             const userRecord = await this.userModel.create({
                 ...userInputDTO,
@@ -22,17 +24,20 @@ export default class AuthService {
                 password,
             });
 
-            if(!userRecord) {
-                throw new Error('User cannot be created');
+            if (!userRecord) {
+                throw new APIError(
+                    HttpStatusCode.SERVER_ERROR,
+                    'User cannot be created',
+                );
             }
 
             // TODO: Send welcome email
-            
+
             const token = this.generateToken(userRecord);
 
             const user = userRecord.toObject();
-            Reflect.deleteProperty(user, "password");
-            Reflect.deleteProperty(user, "salt");
+            Reflect.deleteProperty(user, 'password');
+            Reflect.deleteProperty(user, 'salt');
 
             return { user, token };
         } catch (error) {
@@ -43,21 +48,27 @@ export default class AuthService {
 
     public async SingIn(email: string, password: string): Promise<{ user: IUser, token: string }> {
         try {
-            const userRecord  = await this.userModel.findOne({ email });
-            if(!userRecord){
-                throw new Error("User not registered");
+            const userRecord = await this.userModel.findOne({ email });
+            if (!userRecord) {
+                throw new APIError(
+                    HttpStatusCode.BAD_REQUEST,
+                    'User not registered',
+                );
             }
 
-            const hashedPassword = pbkdf2Sync(password, userRecord.salt, 1000, 64, 'sha512').toString("hex");
-            if(userRecord.password !== hashedPassword){
-                throw new Error("Invalid Password");
+            const hashedPassword = pbkdf2Sync(password, userRecord.salt, 1000, 64, 'sha512').toString('hex');
+            if (userRecord.password !== hashedPassword) {
+                throw new APIError(
+                    HttpStatusCode.UNAUTHORIZED,
+                    'Invalid Password',
+                );
             }
 
             const token = this.generateToken(userRecord);
 
             const user = userRecord.toObject();
-            Reflect.deleteProperty(user, "password");
-            Reflect.deleteProperty(user, "salt");
+            Reflect.deleteProperty(user, 'password');
+            Reflect.deleteProperty(user, 'salt');
 
             return { user, token };
         } catch (error) {
@@ -80,7 +91,7 @@ export default class AuthService {
             },
             config.jwtSecret,
             {
-                algorithm: config.jwtAlgorithm || "HS256"
+                algorithm: config.jwtAlgorithm || 'HS256'
             }
         );
 
